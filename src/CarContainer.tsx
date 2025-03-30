0c538da67888c7147b795572e1316a23dd576eb5
dorosee-cbae <baechandorosee@gmail.com>
2025-03-30 20:15:32 +0900

    사용하지않는 import 제거

--- src/CarContainer.tsx
+++ src/CarContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getImageFiles } from "./utils/getImageFiles";
 import Car360Viewer from "./Car360Viewer";
 
